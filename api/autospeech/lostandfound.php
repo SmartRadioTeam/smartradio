85217981cdc1fc220964badb2edb54e598ee79cb
qwe7002mobile <qwe7002@hotmail.com>
2015-10-12 23:36:58 +0800

    修改编码模式

--- api/autospeech/lostandfound.php
+++ api/autospeech/lostandfound.php
@@ -1,9 +1,10 @@
 <?php
 date_default_timezone_set ('PRC');
 include("../../class/conn.php");
-$sql = "SELECT * FROM `lostandfound`";
-$query = mysql_query($sql,$con);
-while($row=mysql_fetch_array($query)){
-    echo urldecode($row[user])."|".urldecode($row[message])."|".urldecode($row[tel])."|"."失物招领"."|"."06041239"."}";
+//$sql = "SELECT * FROM `lostandfound`";
+$sql = DB_Select("lostandfound");
+$query = DB_Query($sql,$con);
+while($row=DB_Fetch_Array($query)){
+    echo urldecode($row["user"])."|".urldecode($row["message"])."|".urldecode($row["tel"])."|"."失物招领"."|"."06041239"."}";
 }
 ?>
