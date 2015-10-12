8fc4d14fa2490c227ee2f115883dc3cee7546a8e
qwe7002mobile <qwe7002@hotmail.com>
2015-10-12 14:54:38 +0800

    格式化函数

--- touch/index.php
+++ touch/index.php
@@ -5,7 +5,7 @@ $sql = DB_Select("radio");
 $query = DB_Query($sql,$con);
 while($row=DB_Fetch_Array($query)){
 echo '状态：';
-$info=$row[info];
+$info=$row["info"];
 switch ($info){
    case "0":
       echo '<span class="label label-default">未播放</span>';
@@ -18,9 +18,9 @@ switch ($info){
       break;
 }
 echo "<br><br>
-歌曲名：".urldecode($row[name])."<br><br>
-点歌人：".urldecode($row[user])."<br><br>
-送给：".urldecode($row[to])."<br><br>
-最想对TA说:「".urldecode($row[message])."」";
+歌曲名：".urldecode($row["name"])."<br><br>
+点歌人：".urldecode($row["user"])."<br><br>
+送给：".urldecode($row["to"])."<br><br>
+最想对TA说:「".urldecode($row["message"])."」";
 }
 ?>
