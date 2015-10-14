abed9cf307b160984636c0a388e14de6b23e44ae
qwe7002mobile <qwe7002@hotmail.com>
2015-10-14 16:33:23 +0800

    修改sql函数

--- admin/command/backmusic.php
+++ admin/command/backmusic.php
@@ -2,8 +2,9 @@
 include("class_include.php");
 include("toast.php");
 $id=$_POST['id'];
-$sql = "UPDATE `".MYSQLDB."`.`radio` SET `info` = '0' WHERE `radio`.`id` = $id;";
-$result = mysql_query($sql,$con);
+$sql = DB_Update("radio",array("info"=>"='0'"),array("id"=>"=".$id))
+//$sql = "UPDATE `".MYSQLDB."`.`radio` SET `info` = '0' WHERE `radio`.`id` = $id;";
+$result = DB_Query($sql,$con);
     if($result){
         //$sql = "SELECT * FROM `radio` WHERE `radio`.`id` = $id;";
         $sql = DB_Select("radio",array("id"=>"=".$id));
