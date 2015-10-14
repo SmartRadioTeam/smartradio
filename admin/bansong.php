921ae9c2c961276ebfe8ad15d49074d96f84ab95
qwe7002mobile <qwe7002@hotmail.com>
2015-10-14 16:21:41 +0800

    sql语句重写

--- admin/bansong.php
+++ admin/bansong.php
@@ -4,15 +4,15 @@ include("class_include.php");
 <a href="#postmsg" data-toggle="modal" class="btn btn-primary" >添加禁播歌曲</a>
 <div>
 <?php
-include("../../class/conn.php");
-$sql = "SELECT * FROM `ersong`";
-$query = mysql_query($sql,$con);
-while($row=mysql_fetch_array($query)){
+//$sql = "SELECT * FROM `ersong`";
+$sql = DB_Select("ersong");
+$query = DB_Query($sql,$con);
+while($row=DB_Fetch_Array($query)){
     echo '<div class="anime img-thumbnail" id="anime">';
-    echo '歌曲名：'.urldecode($row[name]).'<hr>';
-    echo '<form action="class/del.php" method="post" enctype="multipart/form-data">
-    <input type="hidden" name="id" value="'.$row[id].'">
-    <input type="hidden" name="mod" value="catch">
+    echo '歌曲名：'.urldecode($row["name"]).'<hr>';
+    echo '<form action="command/items.php" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="id" value="'.$row["id"].'">
+    <input type="hidden" name="mod" value="deletecatch">
     <input type="submit" name="submit" class="btn btn-danger" value="删除" />
     </form>';
     echo '<div style="height:1px; margin-top:-1px;clear: both;overflow:hidden;"></div></div>';
