74a2c77030e21a4f5761f2e04f27152583e91ca9
qwe7002 <qwe7002@hotmail.com>
2015-10-14 14:54:04 +0800

    添加警告文本

--- config/command/class_config.php
+++ config/command/class_config.php
@@ -1,5 +1,5 @@
 <?php
-/*类信息*/
+/*类信息(除非您完全知道您在做什么,否则请不要更改此文件)*/
 define("Package_DB", "connect/init.php");
 define("Package_System", "package/system");
 define("Package_Bootstrap", "package/bootstrap");
