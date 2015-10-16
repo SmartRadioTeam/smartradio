8d6ea8c9c3b68ed9e6c7e0f0599a519b976ff248
qwe7002 <qwe7002@hotmail.com>
2015-10-16 10:44:30 +0800

    修改 command

--- api/windowsphone/index.php
+++ api/windowsphone/index.php
@@ -1,10 +1,13 @@
 <?php
-
 include("class_include.php");
 //$sql = "SELECT * FROM `radio`";
 $sql = DB_Select("radio",null,"","*","info");
 $query = DB_Query($sql,$con);
 while($row=DB_Fetch_Array($query)){
-	echo urldecode($row["name"])."｜".urldecode($row["user"])."｜".urldecode($row["to"])."｜"."「".urldecode($row["message"])."」｜".$row["info"]."｜〕";
+	echo urldecode($row["name"])."｜"
+	.urldecode($row["user"])."｜"
+	.urldecode($row["to"])."｜"
+	."「".urldecode($row["message"])."」｜"
+	.$row["info"]."｜〕";
 }
 ?>
