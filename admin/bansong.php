40aaf4ce68319bbf0a9a5284f50dcee3cdca4193
qwe7002mobile <qwe7002@hotmail.com>
2015-10-16 15:55:06 +0800

    修改函数信息

--- admin/bansong.php
+++ admin/bansong.php
@@ -8,14 +8,15 @@ include("class_include.php");
 $sql = DB_Select("ersong");
 $query = DB_Query($sql,$con);
 while($row=DB_Fetch_Array($query)){
-    echo '<div class="anime img-thumbnail" id="anime">';
-    echo '歌曲名：'.urldecode($row["name"]).'<hr>';
-    echo '<form action="command/items.php" method="post" enctype="multipart/form-data">
+    echo '<div class="anime img-thumbnail" id="anime">
+    歌曲名：'.urldecode($row["name"]).'
+    <hr>
+    <form action="command/items.php" method="post" enctype="multipart/form-data">
     <input type="hidden" name="id" value="'.$row["id"].'">
     <input type="hidden" name="mod" value="deletecatch">
     <input type="submit" name="submit" class="btn btn-danger" value="删除" />
-    </form>';
-    echo '<div style="height:1px; margin-top:-1px;clear: both;overflow:hidden;"></div></div>';
+    </form>
+    <div style="height:1px; margin-top:-1px;clear: both;overflow:hidden;"></div></div>';
 
 }
 ?>
