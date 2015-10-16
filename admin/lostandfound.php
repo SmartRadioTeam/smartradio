69b4b7aaf8afd7c278fb71b0e3cd454d658cddc1
qwe7002mobile <qwe7002@hotmail.com>
2015-10-16 16:10:44 +0800

    修改文件路径

--- admin/lostandfound.php
+++ admin/lostandfound.php
@@ -14,7 +14,7 @@ while($row=DB_Fetch_Array($query)){
         联系电话：".urldecode($row["tel"])."<br><br>
         信息：".urldecode($row["message"])."<br><br>
         投稿者ip：".'<a href="http://www.ip138.com/ips138.asp?ip='.urldecode($row["ip"]).'">'.urldecode($row["ip"])."</a><hr>";
-    echo '<form action="command/del.php" method="post" enctype="multipart/form-data">
+    echo '<form action="command/items.php" method="post" enctype="multipart/form-data">
         <input type="hidden" name="id" value="'.$row["id"].'">
         <input type="hidden" name="mod" value="lost">
         <input type="submit" name="submit" class="btn btn-primary" value="删除" />
