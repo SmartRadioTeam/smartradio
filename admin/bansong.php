0e446190998496e3a2cfd99834458cff044413ac
qwe7002mobile <qwe7002@hotmail.com>
2015-10-17 17:37:08 +0800

    修改格式项目

--- admin/bansong.php
+++ admin/bansong.php
@@ -1,7 +1,7 @@
 <?php
 include("class_include.php");
 ?>
-<a href="#postmsg" data-toggle="modal" class="btn btn-primary" >添加禁播歌曲</a>
+<a href="#postmsg" data-toggle="modal" class="btn btn-primary">添加禁播歌曲</a>
 <div>
 <?php
 $sql = DB_Select("bansong");
@@ -15,7 +15,9 @@ while($row=DB_Fetch_Array($query)){
     <input type="hidden" name="mod" value="deletecatch">
     <input type="submit" name="submit" class="btn btn-danger" value="删除" />
     </form>
-    <div style="height:1px; margin-top:-1px;clear: both;overflow:hidden;"></div></div>';
+    <div style="height:1px; margin-top:-1px;clear: both;overflow:hidden;">
+    </div>
+    </div>';
 
 }
 ?>
