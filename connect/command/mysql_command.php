77f459ff845b6bc76909ad12abdb9a2c05fa604a
qwe7002mobile <qwe7002@hotmail.com>
2015-10-17 14:27:12 +0800

    函数修改＝

--- connect/command/mysql_command.php
+++ connect/command/mysql_command.php
@@ -1,42 +1,42 @@
 <?php
 function DB_Insert($table,$arr_values){
-	$id=1;
-	$keys="";
-	$vals="";
+	$id = 1;
+	$keys = "";
+	$vals = "";
 	foreach ($arr_values as $key => $val) {
-		if($id!=count($arr_values)){
-			if($val!=NULL||!is_numeric($val)){
-				$keys=$keys."`".$key."`,";
-				$vals=$vals."'".$val."',";
+		if($id != count($arr_values)){
+			if($val != NULL||!is_numeric($val)){
+				$keys = $keys."`".$key."`,";
+				$vals = $vals."'".$val."',";
 			}else{
-				if($val==NULL&&$val!=0){
-					$val="NULL";
+				if($val == NULL&&$val != 0){
+					$val = "NULL";
 				}
-				$keys=$keys."`".$key."`,";
-				$vals=$vals.$val.",";
+				$keys = $keys."`".$key."`,";
+				$vals = $vals.$val.",";
 			}
 			$id++;
 		}else{
-			if($val!=NULL||!is_numeric($val)){
-				$keys=$keys."`".$key."`";
-				$vals=$vals."'".$val."'";
+			if($val != NULL||!is_numeric($val)){
+				$keys = $keys."`".$key."`";
+				$vals = $vals."'".$val."'";
 			}else{
-				if($val==NULL&&$val!=0){
-					$val="NULL";
+				if($val == NULL&&$val!=0){
+					$val = "NULL";
 				}
-				$keys=$keys."`".$key."`";
-				$vals=$vals.$val;
+				$keys = $keys."`".$key."`";
+				$vals = $vals.$val;
 			}
 		}
 	}
 	return "INSERT INTO `".$table."` (".$keys.") VALUES (".$vals.");";
 }
-function DB_Select($table,$where=null,$limit="",$filter="*",$orderby=null){
+function DB_Select($table,$where = null,$limit = "",$filter = "*",){
 	if($where==null){
 		if($limit!=""){
-			$returnsql = "SELECT ".$filter." FROM `".$table."` LIMIT ".$limit;
+			return "SELECT ".$filter." FROM `".$table."` LIMIT ".$limit;
 		}else{
-			$returnsql = "SELECT ".$filter." FROM `".$table."`";
+			return "SELECT ".$filter." FROM `".$table."`";
 		}
 	}else{
 		$id=1;
@@ -49,15 +49,11 @@ function DB_Select($table,$where=null,$limit="",$filter="*",$orderby=null){
 			}
 		}
 		if($limit!=""){
-			$returnsql= "SELECT ".$filter." FROM `".$table."` WHERE ".$wheres." LIMIT ".$limit.";";
+			return "SELECT ".$filter." FROM `".$table."` WHERE ".$wheres." LIMIT ".$limit.";";
 		}else{
-			$returnsql= "SELECT ".$filter." FROM `".$table."` WHERE ".$wheres.";";
+			return "SELECT ".$filter." FROM `".$table."` WHERE ".$wheres.";";
 		}
 	}
-	if($orderby!=null){
-	   $returnsql .= "ORDER BY ´".$orderby."´";
-	}
-	return $returnsql
 }
 function DB_Delete($table,$where){
 	$id=1;
@@ -87,17 +83,18 @@ function DB_Update($table,$set,$where){
 	return "UPDATE `".$table."` SET ".$sets." WHERE ".$where.";";
 }
 function DB_Query($sql,$con){
-	return mysql_query($sql,$con);
+	return mysqli_query($con,$sql);
 }
 function DB_Fetch_Array($query){
-	return mysql_fetch_array($query);
+	return mysqli_fetch_array($query);
 }
 function DB_Num_Rows($query){
-	return mysql_num_rows($query);
+	return mysqli_num_rows($query);
 }
 function DB_Insert_Id($con){
-	return mysql_insert_id($con);
+	return mysqli_insert_id($con);
 }
 function DB_Error($con){
-	return mysql_error($con);
+	return mysqli_error($con);
 }
+?>
