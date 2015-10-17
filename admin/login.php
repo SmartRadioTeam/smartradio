e17c70d40a3dbcca1cd15376683311e6f55711f8
qwe7002mobile <qwe7002@hotmail.com>
2015-10-17 13:57:23 +0800

    缩进修改

--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,5 @@
 <?php
 include("class_include.php");
-error_reporting(0);
 $username=$_POST['username'];
 $password=$_POST['password'];
 if(!isset($_COOKIE['login'])){
@@ -29,8 +28,11 @@ if(!isset($_COOKIE['login'])){
 }else{
     header('location:index.php');
 }
-echo '<!DOCTYPE html>
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
