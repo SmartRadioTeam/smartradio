7f4c5c6fd367868496ec9807dac0dab45939b7d2
qwe7002mobile <qwe7002@hotmail.com>
2015-10-19 18:35:12 +0800

    修改检查文档

--- api/windowsphone/check.php
+++ api/windowsphone/check.php
@@ -1,6 +1,5 @@
 <?php
 include("class_include.php");
-//$sql = "SELECT * FROM `takeoff` WHERE `id`=0";
 $sql = DB_Select("takeoff",array("id"=>"=0"));
 $query=mysql_query($sql,$con);
 $backcount=mysql_num_rows($query); 
