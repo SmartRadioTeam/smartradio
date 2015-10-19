b738a0fbc05b1f574970b84556199168d2a1d53c
qwe7002mobile <qwe7002@hotmail.com>
2015-10-19 18:39:41 +0800

    配置文件修改（添加时区）

--- config/init.php
+++ config/init.php
@@ -4,3 +4,4 @@ require("command/project_config.php");
 require("command/db_config.php");
 require("command/class_config.php");
 require("command/environmental_constants.php");
+date_default_timezone_set ('PRC');
