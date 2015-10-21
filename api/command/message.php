697e186e66394a85dd64173e01ed47cc4637e1dc
qwe7002mobile <qwe7002@hotmail.com>
2015-10-21 17:28:15 +0800

    json化message输出，修改数据库结构

--- api/command/message.php
+++ api/command/message.php
@@ -1,21 +1,20 @@
 <?php
 include("class_include.php");
 $resultarray[] = array();
-$sql = DB_Select("timetable");
+$sql = DB_Select("setting");
 $query = DB_Query($sql,$con);
 while($row = DB_Fetch_Array($query)){
- //删除时间输出；
-	$row["deltime"];
+ $resultarray["permission"] = $row["permission"];
+ $resultarray["notice"] = $row["notice"];
+	$resultarray["cleantime"] = $row["cleantime"];
 }
-$sql = DB_Select("message");
-$query = DB_Query($sql,$con);
-while($row = DB_Fetch_Array($query)){
-	urldecode($row["message"]);
-}
+
 //todo 失物招领与寻物启事显示模式修改(已经改为两条)。
 $sql = DB_Select("lostandfound",1);
 $query = DB_Query($sql,$con);
 while($row = DB_Fetch_Array($query)){
 $messages = "来自".urldecode($row["user"])."同学的寻物启示：".urldecode($row["message"])."请有拾到者拨打电话：".urldecode($row["tel"])."。谢谢！（本信息将滚动播出，如需了解更多信息请刷新页面。）";
+$resultarray["lostandfound"] = $messages;
 }
+json_encode($resultarray);
 ?>
