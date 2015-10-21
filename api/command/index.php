480443ace35f94cf026ecf69114d581f123c4d25
qwe7002mobile <qwe7002@hotmail.com>
2015-10-21 16:53:46 +0800

    修改info传值方式

--- api/command/index.php
+++ api/command/index.php
@@ -2,14 +2,15 @@
 include("class_include.php");
 $sql = DB_Select("ticket_view",null,"","*","info"); 
 $query = DB_Query($sql,$con);
+$resultarray[] = array();
+$jsonarray[] = array();
 while($row = DB_Fetch_Array($query)){
-echo '状态：';
-$info = $row["info"];
-switch ($info
-echo "
-歌曲名：".urldecode($row["name"])."
-点歌人：".urldecode($row["user"])."
-送给：".urldecode($row["to"])."
-最想对TA说:「".urldecode($row["message"])."」";
+$resultarray["info"] = $row["info"];
+$resultarray["songname"] = urldecode($row["songname"]);
+$resultarray["user"] = urldecode($row["user"]);
+$resultarray["to"] = urldecode($row["to"]);
+$resultarray["message"] = "「".urldecode($row["message"])."」";
+$jsonarray[] = $resultarray;
 }
+json_encode($jsonarray);
 ?>
