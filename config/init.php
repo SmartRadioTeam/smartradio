449a37f23abd254f250df6c8f0ddb225d0423e51
qwe7002mobile <qwe7002@hotmail.com>
2015-10-22 14:23:30 +0800

    修改当前文件常量信息

--- config/init.php
+++ config/init.php
@@ -6,5 +6,5 @@ $json_obj=json_decode($json);
 include("command/project_config.php");
 include("command/db_config.php");
 include("command/class_config.php");
-include("command/environmental_constants.php");
+define("Location_Filename",substr($_SERVER['PHP_SELF'],strrpos($_SERVER['PHP_SELF'], '/')+1));
 date_default_timezone_set ('PRC');
