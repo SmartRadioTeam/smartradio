b4d0227a0f6b73e393ea005e4f69d6d58f8353ad
qwe7002mobile <qwe7002@hotmail.com>
2015-10-22 13:47:32 +0800

    修改setting文件位置

--- config/init.php
+++ config/init.php
@@ -1,6 +1,6 @@
 <?php
 //error_reporting(0);//(错误提示，开发模式下为注释)
-$json = file_get_contents("config/setting.json");
+$json = file_get_contents("setting.json");
 $json_obj=json_decode($json);
 //生成对象json_obj
 include("command/project_config.php");
