c781229067aa9494f2001a8c45de4c2c2fec347a
qwe7002(windows) <381511791@qq.com>
2015-11-06 16:51:06 +0800

    修改lostandfound输出方式

--- api/command/message.php
+++ api/command/message.php
@@ -12,7 +12,8 @@ $sql = DB_Select("lostandfound",null,1);
 $query = DB_Query($sql,$con);
 while($row = DB_Fetch_Array($query)){
 	$messages = "来自".urldecode($row["user"])."同学的寻物启示：".urldecode($row["message"])."请有拾到者拨打电话：".urldecode($row["tel"])."。谢谢！（本信息将滚动播出，如需了解更多信息请刷新页面。）";
-	$resultarray["lostandfound"] = $messages;
+	$lostandfoundarr[] = $messages;
 }
+$resultarray["lostandfound"] = $lostandfoundarr;
 json_encode($resultarray,JSON_UNESCAPED_UNICODE);
 ?>
