9ae602dbb99b6b45861fec7f7b074aa587c0c54c
qwe7002(home) <qwe7002@hotmail.com>
2015-11-13 19:58:00 +0800

    修改登录逻辑

--- admin/login.php
+++ admin/login.php
@@ -3,20 +3,22 @@ include("class_include.php");
 if(isset($_POST['username'])){
   $username = $_POST['username'];
   $password = $_POST['password'];
-  if(!isset($_COOKIE['login'])){
-    $username=md5($username);
-    $password=md5($password);
-    $sql = DB_Select("adminuser",array("usermd5"=>"='".$username."'"));
-    $query = DB_Query($sql,$con);
-    while($row=DB_Fetch_Array($query)){
-      if($password == $row["password"]){
-          setcookie('login','sanmingxueyuan',time()+86400,"/");
-          header('location:/admin/');
-          exit();
-      }else{
-          $message = '您的密码输入错误，请重新输入！'; 
-          break;
-      }
+  if(isset($_COOKIE['login'])){
+    header('location:/admin/');
+    exit();
+  }
+  $username=md5($username);
+  $password=md5($password);
+  $sql = DB_Select("adminuser",array("usermd5"=>"='".$username."'"));
+  $query = DB_Query($sql,$con);
+  while($row=DB_Fetch_Array($query)){
+    if($password == $row["password"]){
+        setcookie('login','sanmingxueyuan',time()+86400,"/");
+        header('location:/admin/');
+        exit();
+    }else{
+        $message = '您的密码输入错误，请重新输入！'; 
+        break;
     }
   }
 }
