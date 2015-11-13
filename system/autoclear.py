67990e92f5fd0ac59a5dcafb0255690afc8f4fa7
qwe7002(home) <qwe7002@hotmail.com>
2015-11-13 22:39:05 +0800

    修改配置文件

--- system/autoclear.py
+++ system/autoclear.py
@@ -4,16 +4,16 @@ import MySQLdb
 import time
 import sys
 import json
-#导入config/setting.json(TODO)
+#导入config/setting.json
 f = open("../config/setting.json","r")
 content = f.readline() 
 jsonVal = json.loads(content)
-conn= MySQLdb.connect(
-        host=jsonVal['DB_Host'],
+conn = MySQLdb.connect(
+        host = jsonVal['DB_Host'],
         port = 3306,
-        user=jsonVal['DB_User'],
-        passwd=jsonVal['DB_Password'],
-        db =jsonVal['DB_Name'],
+        user = jsonVal['DB_User'],
+        passwd = jsonVal['DB_Password'],
+        db = jsonVal['DB_Name'],
         )
 cur = conn.cursor()
 #只删除显示表信息，保留日志表信息(减少读取时间)
