55bd2158b508ad7b9b43fb881babcda07753df1d
qwe7002(home) <qwe7002@hotmail.com>
2015-11-14 22:04:32 +0800

    提交格式修改

--- admin/login.php
+++ admin/login.php
@@ -13,7 +13,7 @@ if(isset($_POST['username'])){
   $query = DB_Query($sql,$con);
   while($row=DB_Fetch_Array($query)){
     if($password == $row["password"]){
-        setcookie('login','sanmingxueyuan',time()+86400,"/");
+        setcookie('login',$row["user"],time()+86400,"/");
         header('location:/admin/');
         exit();
     }else{
@@ -25,8 +25,8 @@ if(isset($_POST['username'])){
 echo '
 <!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -38,20 +38,21 @@ echo '
     <div class="container">
       <form class="form-signin" role="form" method="post">
         <h2 class="form-signin-heading">'.Project_Name.'管理用户登录</h2>';
-          if(isset($message)){
-            echo '<div class="alert alert-danger" role="alert">';
-            echo $message;
-            echo '</div>';
-          }
-          echo '
+      if(isset($message)){
+        echo '<div class="alert alert-danger" role="alert">';
+        echo $message;
+        echo '</div>';
+      }
+echo '
         <input type="text" name="username" class="form-control" placeholder="用户名" required="" autofocus="">
         <input type="password" name="password" class="form-control" placeholder="密码" required="">
         <button class="btn btn-lg btn-primary btn-block" type="submit">登录</button>
       </form>
     </div>
-      <div class="footer">
+    <div class="footer">
       <div class="container2">
         <p class="text-muted">睿欧科技有限公司</p>
       </div>
     </div>
-</body></html>';
+  </body>
+</html>';
--- admin/template/head.php
+++ admin/template/head.php
@@ -50,7 +50,14 @@
                 <li<?php if(Location_Filename == "index.php" && $_GET['mode'] == "search"){echo ' class="active"';}?>><a href="#today"data-toggle="modal">点播搜索</a></li>
                 <li<?php if(Location_Filename == "lostandfound.php"){echo ' class="active"';}?>><a href="lostandfound.php">寻物启示</a></li>
                 <li><a href="#off" data-toggle="modal">系统设置</a></li>
-                <li><a href="command/outlogin.php">退出</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span><?php echo $_COOKIE['login']; ?><span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="command/outlogin.php">退出</a></li>
+                    </ul>
+                </li>
             </ul>
         </div>
     </div>
