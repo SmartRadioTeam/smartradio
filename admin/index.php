2e9a1e8d77b967ab597d86edaf17224d2006c238
qwe7002(home) <qwe7002@hotmail.com>
2015-11-14 23:25:49 +0800

    Revert "修改post提交"
    This reverts commit 99630344e96e8f1f1a0e3f04ec3ab5f347619031.

--- admin/index.php
+++ admin/index.php
@@ -23,6 +23,11 @@ while($row = DB_Fetch_Array($query)){
 	frame($row["id"],$row["info"],$row["uptime"],$row["time"],$row["option"],$row["name"],$row["user"],$row["to"],$row["message"],$row["ip"],$mode);
 }
 ?>
+<form name="change" action="command/items.php" method="post">
+    <input type="hidden" name="id">
+    <input type="hidden" name="mod">
+    <input type="hidden" name="location" value="<?php echo $mode?>">
+</form>
 <script>
 function changeform(ids,mode){
   document.change.id.value=ids;
