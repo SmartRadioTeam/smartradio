6a3acc31c57696c60921a5b2426397c9919a2c9f
qwe7002(windows) <381511791@qq.com>
2015-11-17 16:39:02 +0800

    修改为数组输出

--- api/command/message.php
+++ api/command/message.php
@@ -11,8 +11,9 @@ while($row = DB_Fetch_Array($query)){
 $sql = DB_Select("lostandfound",null,1);
 $query = DB_Query($sql,$con);
 while($row = DB_Fetch_Array($query)){
-	$messages = "来自".urldecode($row["user"])."同学的寻物启示：".urldecode($row["message"])."请有拾到者拨打电话：".urldecode($row["tel"])."。谢谢！（本信息将滚动播出，如需了解更多信息请刷新页面。）";
-	$resultarray["lostandfound"] = $messages;
+	$messages[] = "来自".urldecode($row["user"])."同学的寻物启示：".urldecode($row["message"])."请有拾到者拨打电话：".urldecode($row["tel"])."。谢谢！（本信息将滚动播出，如需了解更多信息请刷新页面。）";
+	
 }
+$resultarray["lostandfound"] = $messages;
 echo json_encode($resultarray,JSON_UNESCAPED_UNICODE);
 ?>
