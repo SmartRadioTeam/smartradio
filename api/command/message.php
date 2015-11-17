55091daa7b68d127ac14e369107558ef82947652
qwe7002(windows) <381511791@qq.com>
2015-11-17 16:41:04 +0800

    输出全部

--- api/command/message.php
+++ api/command/message.php
@@ -8,7 +8,7 @@ while($row = DB_Fetch_Array($query)){
 	$resultarray["cleantime"] = urldecode($row["cleantime"]);
 	break;
 }
-$sql = DB_Select("lostandfound",null,1);
+$sql = DB_Select("lostandfound");
 $query = DB_Query($sql,$con);
 while($row = DB_Fetch_Array($query)){
 	$messages[] = "来自".urldecode($row["user"])."同学的寻物启示：".urldecode($row["message"])."请有拾到者拨打电话：".urldecode($row["tel"])."。谢谢！（本信息将滚动播出，如需了解更多信息请刷新页面。）";
