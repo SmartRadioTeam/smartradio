34a823bde9089c8b4cc95a649dc4d8b9979b8d86
qwe7002(windows) <381511791@qq.com>
2015-11-18 20:49:13 +0800

    修改case

--- admin/command/class_include.php
+++ admin/command/class_include.php
@@ -7,9 +7,6 @@ include("../../config/init.php");
 include("../../connect/init.php");
 include("../../package/system/messagebox/messagebox.php");
 switch(Location_Filename){
-	case "backmusic.php":
-		include("toast.php");
-		break;
 	case "items.php":
 		include("toast.php");
 		break;
