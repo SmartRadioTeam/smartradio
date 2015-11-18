9ad79fe8a0152e7fb2a9196f130cc545ebd4d9a0
qwe7002(windows) <381511791@qq.com>
2015-11-18 21:00:35 +0800

    修复bug

--- admin/command/class_include.php
+++ admin/command/class_include.php
@@ -1,9 +1,9 @@
 <?php
+include("../../config/init.php");
 if(!isset($_COOKIE['login']) && Location_Filename != "login.php"){
 	header("location:../login.php");
 	exit();
 }
-include("../../config/init.php");
 include("../../connect/init.php");
 include("../../package/system/messagebox/messagebox.php");
 switch(Location_Filename){
