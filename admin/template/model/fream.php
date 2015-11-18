8ea60225b6cc051597d382272a373514297f709c
qwe7002(windows) <381511791@qq.com>
2015-11-18 13:05:26 +0800

    修改拼写错误

--- admin/template/model/fream.php
+++ admin/template/model/fream.php
@@ -16,7 +16,7 @@ function frame($id,$info,$uptime,$time,$option,$songid,$user,$to,$message,$ip,$m
         echo "<br><br>
         提交时间：".urldecode($uptime)."<br><br>
         希望播放的时间：".str_replace('-', '月', urldecode($time))."日 ".urldecode($option)."<br><br>
-        歌曲名：<a href=\"javascript:musciplay('".$musicarray[$songid]["songurl"]."');\">".$musicarray[$songid]["songtitle"]."</a><br><br>
+        歌曲名：<a href=\"javascript:musicplay('".$musicarray[$songid]["songurl"]."');\">".$musicarray[$songid]["songtitle"]."</a><br><br>
         点歌人：".urldecode($user)."<br><br>
         送给：".urldecode($to)."<br><br>
         留言：".urldecode($message)."<br><br>
