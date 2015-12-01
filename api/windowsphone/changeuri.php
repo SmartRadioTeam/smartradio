77575ab00fa26b7ad610add1fd6cd7c0a1b4cf9f
qwe7002mobile <qwe7002@hotmail.com>
2015-12-01 09:09:35 +0800

    停止追踪

--- api/windowsphone/changeuri.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("class_include.php");
-$old=$_GET['old'];
-$new=$_GET['new'];
-$new=urlencode($new);
-$old=urlencode($old);
-$sql = "UPDATE `radio` SET `uri` = '$new' WHERE `uri` = '".$old."';";
-$result = mysql_query($sql,$con);
-?>	
