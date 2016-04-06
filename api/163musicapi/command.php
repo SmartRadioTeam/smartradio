87c17acae0b55655d9c962e690bb2e3048055aa4
qwe7002(mobile) <qwe7002@hotmail.com>
2016-04-06 18:49:52 +0800

    网易云音乐获取逻辑修改

--- api/163musicapi/command.php
+++ api/163musicapi/command.php
@@ -2,7 +2,7 @@
 function curl_get($url)
 {
     $refer = "http://music.163.com/";
-    $header[] = "Cookie: " . "appver=1.9.2.109452;";
+    $header[] = "Cookie: appver=1.9.2.109452;";
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
