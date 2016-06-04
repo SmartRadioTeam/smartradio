2f18c642ed3a21f2178aafe57d9f6af5bd9bc380
qwe7002(windows) <381511791@qq.com>
2016-06-04 17:02:48 +0800

    修改安装模块

--- api/admin_control/install.php
+++ api/admin_control/install.php
@@ -3,7 +3,9 @@
 include 'class_include.php';
 if (!$redis->exists("settings"))
 {
-	$redis->SET("settings", "{'notice':'欢迎使用Smartradio','permission','0'}");
+	$settings["notice"]="欢迎使用smartradio!";
+	$settings["permission"]="0";
+	$redis->SET("settings", json_encode($settings));
 }
 if (!$redis->exists('lostandfound'))
 {
@@ -22,3 +24,4 @@ if (!$redis->exists('songinfo'))
 	$resultarray["songinfo"] = "[]";
 	$redis->SET("songinfo", "[]");
 }
+$redis->save();
